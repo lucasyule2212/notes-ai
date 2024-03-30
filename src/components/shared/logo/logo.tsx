89cec87b2a327097338ac1aed741c524475e1554
Lucas Yule <lyrma@cin.ufpe.br>
2024-03-29 21:20:43 -0300

    feat(logo-component): adding new app logo component

---
+++ src/components/shared/logo/logo.tsx
@@ -0,0 +1,12 @@
+const Logo = () => {
+  return (
+    <div className="group flex justify-center space-x-1">
+      <h1 className="gradient-text font-cookie text-5xl">Canva</h1>
+      <h1 className="bg-black bg-[length:200%_100%] bg-clip-text text-4xl font-bold transition-colors group-hover:animate-shimmer group-hover:bg-[linear-gradient(110deg,#000103,45%,#7a7f88,55%,#000103)] group-hover:text-transparent">
+        AI
+      </h1>
+    </div>
+  )
+}
+
+export default Logo
